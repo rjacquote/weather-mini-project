eecfc27380fdf67681d39eb474fca27b1713edd9
Daniel Kuhn <dkuhn@pgh.yearup.org>
2022-12-02 15:26:52 -0500

    add data from other branch, finish up code and fix conficts

--- scripts/weatherinfo.js
+++ scripts/weatherinfo.js
@@ -2,37 +2,48 @@
 //temp city data
 let table = document.getElementById('userTable');
 //will work as long as the city latitude and longitiude are valid
-let cities = [
-    {
-        "city": "Denver",
-        "state": "Colorado",
-        "latitude": 39.8526406,
-        "longitude": -105.0493497
-    },
-    {
-        "city": "Houston",
-        "state": "Texas",
-        "latitude": 29.8174782,
-        "longitude": -95.6814916
-    },
-    {
-        "city": "Test Value",
-        "state": "Texas",
-        "latitude": 32.6791,
-        "longitude": -97.4641
-    }
-]
+// let cities = [
+//     {
+//         "city": "Denver",
+//         "state": "Colorado",
+//         "latitude": 39.8526406,
+//         "longitude": -105.0493497
+//     },
+//     {
+//         "city": "Houston",
+//         "state": "Texas",
+//         "latitude": 29.8174782,
+//         "longitude": -95.6814916
+//     },
+//     {
+//         "city": "Test Value",
+//         "state": "Texas",
+//         "latitude": 32.6791,
+//         "longitude": -97.4641
+//     }
+// ]
+fetch('../scripts/locations.json')
+    .then((response) => response.json())
+    .then(cities =>{
+
 let locatMenu = document.getElementById('cityName');
 let locatAmount = cities.length;
 for (let i = 0; i < locatAmount; i++) {
     let option = new Option(cities[i].city);
     locatMenu.appendChild(option);
-};
-let amount = cities.length
+ 
+};})
+
 
 
 function citySelect() {
     
+    
+
+    fetch('../scripts/locations.json')
+    .then((response) => response.json())
+    .then(cities =>{
+    let amount = cities.length
     var e = document.getElementById("cityName");
     var text = e.options[e.selectedIndex].text;
     console.log(text)
@@ -67,17 +78,63 @@ console.log(testUrl)
     
     //loop function to create a table
 
+
+
+        let listTime = document.getElementById('showTime')
+        let listImg = document.getElementById('showImg')
+        let listWeather = document.getElementById('showWeather')
+        let listWind = document.getElementById('showWind')
+        let listTemps = document.getElementById('showTemps')
+            
+        while (listTime.firstChild) {
+            listTime.removeChild(listTime.firstChild);
+        }
+        while (listImg.firstChild) {
+            listImg.removeChild(listImg.firstChild);
+        }
+        while (listWeather.firstChild) {
+            listWeather.removeChild(listWeather.firstChild);
+        }
+        while (listWind.firstChild) {
+            listWind.removeChild(listWind.firstChild);
+        }
+        while (listTemps.firstChild) {
+            listTemps.removeChild(listTemps.firstChild);
+        }
     for(i=0; i < forecastArray.length; i++){
-            let row=table.insertRow(-1);
-            let cell1 = row.insertCell(0);
-            let cell2 = row.insertCell(1);
-            let cell3 = row.insertCell(2);
-            let cell4 = row.insertCell(3);
-            cell1.innerHTML = data.properties.periods[i].name;
-            cell2.innerHTML = "Temperature " + data.properties.periods[i].temperature + " " +  data.properties.periods[i].temperatureUnit;
-            cell3.innerHTML = "Winds" + data.properties.periods[i].windDirection + " " + data.properties.periods[i].windSpeed;
-            cell4.innerHTML = data.properties.periods[i].shortForecast;
+            // let row=table.insertRow(-1);
+            // let cell1 = row.insertCell(0);
+            // let cell2 = row.insertCell(1);
+            // let cell3 = row.insertCell(2);
+            // let cell4 = row.insertCell(3);
+            // cell1.innerHTML = data.properties.periods[i].name;
+            // cell2.innerHTML = "Temperature " + data.properties.periods[i].temperature + " " +  data.properties.periods[i].temperatureUnit;
+            // cell3.innerHTML = "Winds" + data.properties.periods[i].windDirection + " " + data.properties.periods[i].windSpeed;
+            // cell4.innerHTML = data.properties.periods[i].shortForecast;
+
+            
+    
+            let nameTime = document.createElement('th')
+            listTime.appendChild(nameTime).innerText = data.properties.periods[i].name;
+
+            
+            let fitImg = document.createElement('th')
+            listImg.appendChild(fitImg)
+            let nameImg = document.createElement('img')
+            fitImg.appendChild(nameImg).src = data.properties.periods[i].icon;
+            
+            
+            let nameWeather = document.createElement('th')
+            listWeather.appendChild(nameWeather).innerText = data.properties.periods[i].shortForecast;
+            
+            
+            let nameWind = document.createElement('th')
+            listWind.appendChild(nameWind).innerText = "Winds " + data.properties.periods[i].windDirection + " " + data.properties.periods[i].windSpeed;
+            
+            
+            let nameTemps = document.createElement('th')
+            listTemps.appendChild(nameTemps).innerText = data.properties.periods[i].temperature + " " + data.properties.periods[i].temperatureUnit;
         }
     }))})
     break}}
-}
+})}
