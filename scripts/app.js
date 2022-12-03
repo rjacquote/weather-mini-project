950ae05c97cf49fc0fd4d86a178f2bef784ab986
Sam Bhandari <sambhandari2021@yahooe.com>
2022-12-02 20:58:24 -0500

    add city array back

--- scripts/app.js
+++ scripts/app.js
@@ -4,8 +4,6 @@ const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
 
 
-
-
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
